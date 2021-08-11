ffb78767c202180a606de925cbba2ca9a70fbfaf
Jasonzhang <jasonzhang176301@gmail.com>
2021-08-11 10:46:26 +0800

    2021.8.11 update

--- AIGO/week3/Medium_19.py
+++ AIGO/week3/Medium_19.py
@@ -1,5 +1,6 @@
 # 翻转链表的倒数第N个节点
 
+
 # 定义链表类
 class ListNode(object):
     def __init__(self, val=0, next=None):
@@ -7,9 +8,31 @@ class ListNode(object):
         self.next = next
 
 
+# 快慢指针法，先让快指针走N + 1步，然后快慢指针一起走，最后慢指针指向待删除节点的前驱节点
+# 第一个重点：快慢指针相差N + 1步，方便进行删除操作
+# 第二个重点：由于需要对头节点进行删除操作，因此需要新建一个虚拟头节点dummy，用于删除头节点
+# 第三个重点：最后的返回值是dummy.next，而不是head，因为head有可能会被删除
 def removeNthFromEnd(head, n):
-    return 0
+    dummy = ListNode(next=head)
+    fast = dummy
+    slow = dummy
+    for i in range(n + 1):
+        fast = fast.next
+    while fast:
+        fast = fast.next
+        slow = slow.next
+    slow.next = slow.next.next
+    return dummy.next
 
 
 if __name__ == '__main__':
-    print(removeNthFromEnd(1, 3))
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+    head = removeNthFromEnd(head, 1)
+    while head:
+        print(head.val)
+        head = head.next
+
--- Job/Starting Plan/day5/Easy_876.py
+++ Job/Starting Plan/day5/Easy_876.py
@@ -7,11 +7,21 @@ class ListNode(object):
         self.val = val
         self.next = next
 
+
+# 使用快慢指针，快指针每次走两步，慢指针每次走一步，快指针到达终点时，慢指针指向中间结点
 def middleNode(head):
-    return None
+    fast, slow = head, head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    return slow.val
 
 
 if __name__ == '__main__':
-    head = ListNode()
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+    print(middleNode(head))
 
-    print(middleNode())

a3541a2d233b789ed675d1a1a24d4403d6f27eb4
Jasonzhang <jasonzhang176301@gmail.com>
2021-08-23 22:35:29 +0800

    2021.8.23 update

---
+++ AIGO/week7/U_Hard_312.py
@@ -0,0 +1,22 @@
+# 戳气球
+
+
+def maxCoins(nums):
+    if not nums:
+        return 0
+    if len(nums) == 1:
+        return nums[0]
+    nums = [1] + nums + [1]
+    n = len(nums)
+    dp = [[0] * n for _ in range(n)]
+    for i in range(n - 1, -1, -1):
+        for j in range(i + 2, n):
+            for k in range(i + 1, j):
+                dp[i][j] = max(dp[i][j], dp[i][k] + dp[k][j] + nums[i] * nums[k] * nums[j])
+                print(dp)
+    return dp[0][-1]
+
+
+if __name__ == '__main__':
+    nums = [3, 1, 5, 8]
+    print(maxCoins(nums))
---
+++ AIGO/week7/U_Hard_85.py
@@ -0,0 +1,11 @@
+# 最大矩形
+
+
+def maxRectangle(matrix):
+    pass
+
+
+if __name__ == '__main__':
+    matrix = [["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"],
+              ["1", "0", "0", "1", "0"]]
+    print(maxRectangle(matrix))

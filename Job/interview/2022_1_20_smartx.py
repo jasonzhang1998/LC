9d2979b2a38e606e667c20e0bd2002a8603b64db
Jasonzhang <jasonzhang176301@gmail.com>
2022-01-21 09:15:09 +0800

    2022.1.21 update

---
+++ Job/interview/2022_1_20_smartx.py
@@ -0,0 +1,25 @@
+class RandomList:
+    def __init__(self, val=0, next=None, random=None):
+        self.val = val
+        self.next = next
+        self.random = random
+
+
+
+
+class Solution:
+    def copyList(self, head):
+        dic = {}
+        node =head
+        while head:
+            dic[head] = RandomList(head.val)
+            head = head.next
+
+        for k, v in dic.items():
+            v.next = dic.get(k.next)
+            v.random = dic.get(k.random)
+        return dic[node]
+
+
+
+

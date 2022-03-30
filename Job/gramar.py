1b24a80917fb6677bdb4bb55e8634be879722548
zhangxiong <jasonzhang176301@gmail.com>
2022-03-30 10:26:53 +0800

    tes

--- Job/gramar.py
+++ Job/gramar.py
@@ -1,2 +1,18 @@
-s = 'fsdd'
-print(s * 3)
+def partition(arr, left, right):
+    if left >= right:
+        return
+    i, j = left, right
+    while i < j:
+        while i < j and arr[j] >= arr[left]:
+            j -= 1
+        while i < j and arr[i] <= arr[left]:
+            i += 1
+        arr[i], arr[j] = arr[j], arr[i]
+        print(arr)
+    arr[i], arr[left] = arr[left], arr[i]
+    return arr
+
+
+arr = [5, 2, 3, 4, 1, 3, 47, 8, 9]
+
+partition(arr, 0, len(arr) - 1)

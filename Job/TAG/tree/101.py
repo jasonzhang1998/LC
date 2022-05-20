47ecb9c2dc7f63b3e72b77ab836bb926d0d4cbfe
zhangxiong <jasonzhang176301@gmail.com>
2022-05-20 10:53:50 +0800

    vscode git

---
+++ Job/TAG/tree/101.py
@@ -0,0 +1,14 @@
+# 对称二叉树
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        pass
+

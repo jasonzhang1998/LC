997477a20e7deebfa1b9bac0d53ec33a60b6089d
zhangxiong <jasonzhang176301@gmail.com>
2022-05-31 19:17:36 +0800

    test extension host

---
+++ Job/Contest/2022_5_29/2289.py
@@ -0,0 +1,16 @@
+# 使数组按非递减顺序排列
+
+from typing import List
+
+
+class Solution:
+    def totalSteps(self, nums: List[int]) -> int:
+        pass
+
+
+
+
+
+
+nums = [5, 3, 4, 4, 7, 3, 6, 11, 8, 5, 11]
+print('hello')

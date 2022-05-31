422e615a9c03ef66b2b1dd7eeedfa231f749dd7e
zhangxiong <jasonzhang176301@gmail.com>
2022-05-31 19:38:10 +0800

    19:38

--- Job/Contest/2022_5_29/2289.py
+++ Job/Contest/2022_5_29/2289.py
@@ -1,8 +1,15 @@
 # 使数组按非递减顺序排列
 
+from curses.ascii import SO
 from typing import List
 
 
 class Solution:
     def totalSteps(self, nums: List[int]) -> int:
         pass
+
+
+nums = [5, 3, 4, 4, 7, 3, 6, 11, 8, 5, 11]
+
+print(Solution().totalSteps(nums))
+

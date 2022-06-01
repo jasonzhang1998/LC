5a04ceb60f197408ff04b7d6a84585a7e491bfce
zhangxiong <jasonzhang176301@gmail.com>
2022-06-01 14:35:51 +0800

    2022.6.1 update

--- Job/Contest/2022_5_29/2289.py
+++ Job/Contest/2022_5_29/2289.py
@@ -1,16 +1,23 @@
 # 使数组按非递减顺序排列
 
-from curses.ascii import SO
 from typing import List
 
 
 class Solution:
+    # 单调栈解法
     def totalSteps(self, nums: List[int]) -> int:
-        pass
+        ans, stack = 0, []
+        for num in nums:
+            max_t = 0
+            while stack and stack[-1][0] <= num:
+                max_t = max(max_t, stack.pop()[1])
+            max_t = max_t + 1 if stack else 0
+            ans = max(ans, max_t)
+            stack.append((num, max_t))
+        return ans
+
 
 
 nums = [5, 3, 4, 4, 7, 3, 6, 11, 8, 5, 11]
 
 print(Solution().totalSteps(nums))
-
-print('hello')

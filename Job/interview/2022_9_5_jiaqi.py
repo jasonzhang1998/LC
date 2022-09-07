f3f3615f4c8f567b4636a7bce4a9efba1a554d65
Jasonzhang <jasonzhang176301@gmail.com>
2022-09-07 20:35:29 +0800

    2022.9.5 jiaqi interview update

--- Job/interview/2022_9_5_jiaqi.py
+++ Job/interview/2022_9_5_jiaqi.py
@@ -20,11 +20,11 @@ def getMinSumK(nums, k):
     # dp[i]表示以nums[i]结尾的最小子数组和的大小
     dp = [0] * n
     dp[0] = nums[0]
-    dp[k - 1] = sum(nums[:k])
-    for i in range(k, n):
-        # if i < k:
-        #     dp[i] = min(dp[i - 1] + nums[i], nums[i])
-        #     continue
+    # dp[k - 1] = sum(nums[:k])
+    for i in range(1, n):
+        if i < k:
+            dp[i] = min(dp[i - 1] + nums[i], nums[i])
+            continue
         dp[i] = min(dp[i - k], 0) + sum(nums[i - k + 1:i + 1])
     return min(dp[k - 1:])
 
@@ -32,5 +32,5 @@ def getMinSumK(nums, k):
 
 nums = [-18, 3, 4, -5, -1, 4]
 # print(getMinSum(nums))
-print(getMinSumK(nums, 1))
+print(getMinSumK(nums, 6))
 
